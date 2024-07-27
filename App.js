af624a293e4065367d2dabb8345c9e58e1012c82
pandeyanurag2155 <anuragshanker143@gmail.com>
2024-07-27 11:45:59 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
-/* <div id="parent">
-<div id="child">
-<h1> I'm h1 tag </h1>
-<h2> I'm h2 tag </h2>
-</div>
-</div> */
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 
 // React Element(object)=> HTML(Browser Understand)
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h1", {}, "this is namaste react "),
     React.createElement("h2", {}, "I'm an h2 tag"),
   ]),
 
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 #namaste react
+
+# Parcel
+-Dev Build
+-Local server
+-HMR=> Hot Module replacement
+-File Watching algorithm - written in c++
+-caching- Faster Builds
+-IMage optimization
+-minification
+-Bundling 
+-Compressing
+-consistent Hashing
+-Code splitting
+-Differential Bundling =>support older -Browsers
+-diagnostic
+-error handling
+-HTTPs
+-Tree Shaking =>Remove unused code for you 
+-Different dev and prod bundles
