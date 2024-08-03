ebb210e0e93cef857083dbd1527d23cd62e133d8
pandeyanurag2155 <anuragshanker143@gmail.com>
2024-08-03 22:40:59 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,34 +1,72 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// // React Element(object)=> HTML(Browser Understand)
 
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "this is namaste react "),
+//     React.createElement("h2", {}, "I'm an h2 tag"),
+//   ]),
 
-// React Element(object)=> HTML(Browser Understand)
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I'm an h1 tag"),
+//     React.createElement("h2", {}, "I'm an h2 tag"),
+//   ]),
+// ]);
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "this is namaste react "),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
+// //JSX
 
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-]);
+// // const heading = React.createElement(
+// //   "h1",
+// //   { id: "heading" },
+// //   "Hello world from React!"
+// // );
+// // console.log(heading);
+// console.log(parent);
 
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-//JSX
+// root.render(parent);
 
- 
+//React element =>object =>HTMLElement(render)
+
 // const heading = React.createElement(
 //   "h1",
 //   { id: "heading" },
-//   "Hello world from React!"
-// );
-// console.log(heading);
-console.log(parent);
+//   "Namaste React"
+//   );
 
+// console.log(heading)
+
+//jsx-HTML-like or XML-like syntax
+//React element
+
+const elem=<span>React element</span>
+const jsxHeading = (
+<h1 className="head" tabIndex="5">
+{elem}
+Namaste React using Jsx
+</h1>
+);
+// const Title = ()=> (
+//      <h1 className="head" tabIndex="5">
+//      Namaste React using Jsx
+//      </h1>
+//      );
+
+//React Component
+//class Based component - old way of writing code
+//functional component - New way of writing code
+
+//React Functional Component
+//component composition
+const HeadingComponent = () =>(
+    <div id ="container">
+    {/* <Title /> */ jsxHeading}
+        <h1 className="heading">Namaste React Functional Component</h1>
+    </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent/>);
